6d74b331e481bea8ff96adcbe2039b0d936b527b
Sebastien THIEBAUD <sebastien.thiebaud@supinfo.com>
2012-12-17 17:08:40 -0800

    Initial commit

---
+++ STScratchView/STScratchView.h
@@ -0,0 +1,13 @@
+//
+//  STScratchView.h
+//  STScratchView
+//
+//  Created by Sebastien Thiebaud on 12/17/12.
+//  Copyright (c) 2012 Sebastien Thiebaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STScratchView : UIView
+
+@end
---
+++ STScratchViewExample/STScratchViewExample/STViewController.h
@@ -0,0 +1,13 @@
+//
+//  STViewController.h
+//  STScratchViewExample
+//
+//  Created by Sebastien Thiebaud on 12/17/12.
+//  Copyright (c) 2012 Sebastien Thiebaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STViewController : UIViewController
+
+@end
